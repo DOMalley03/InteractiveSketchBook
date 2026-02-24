4fa81cd306e3458735c7fa5afbaaa6af410469db
DOMalley03 <domalley@sfsu.edu>
2026-02-24 15:06:36 -0800

    added mult to project

--- MicroProject1/js/p5Sketch.js
+++ MicroProject1/js/p5Sketch.js
@@ -1,29 +1,58 @@
 let points=0;
 let mult = 1;
+let upgradeCost= 20;
 function setup(){
     createCanvas(600, 400);
-    
+   
     
 }
 function draw() {
-    background(100);
+ background(100);
 
-    //circle
+    //Circle
     fill('red');
     circle(width / 2, height / 2, 250);
 
+    //Press Me
     fill('white');
     textSize(32);
     textAlign(CENTER, CENTER);
     text('Press Me', 300, 200);
 
+    //Score
     fill('white');
-    textSize(30);
+    textSize(18);
     textAlign(LEFT, TOP);
-    text('Score: ' + points, 0, 0);
+    text('Score: ' + points, 10, 10);
+    text('Multiplier: ' + mult + 'x', 10, 30);
+
+    //upgrade
+    fill(50, 200, 50);
+    rect(400, 300, 170, 60, 10);
+
+    fill('white');
+    textAlign(CENTER,CENTER);
+    textSize(18);
+    text('Upgrade Cost: ' + upgradeCost, 485, 330);
 
 }
 
 function mousePressed(){
-    points += mult;
+    //distance between mouse and center of circle
+    let d= dist(mouseX, mouseY, width/2, height/2);
+
+    if (d<125){
+        points += mult;
+    }
+
+    //if mouse clicks on upgrade
+    if(mouseX > 400 && mouseX <570 && mouseY >300 && mouseY <360) {
+            if (points >= upgradeCost){
+                points -= upgradeCost;
+                mult++;
+                upgradeCost = floor(upgradeCost * 1.5);
+            }
+
+        }
+
 }
